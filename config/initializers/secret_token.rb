acafec157c281c78936685840840bb14e1ae634b
Samuel Mullen <samullen@unknownb88d120094f8.gateway.2wire.net>
2012-03-07 17:09:04 -0600

    initial offering

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArExplain::Application.config.secret_token = 'd2dd6434b25e6cf0950e57f9e3cc588666eb8305df6eaaeed37b415d70331eaa0e621a3c100e04b8c932211ac1d1e5bd6505fa1ed95f4a007ea31e011b10d7d0'
