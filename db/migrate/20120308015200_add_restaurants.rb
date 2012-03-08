22f396d2aea4d32770b605793157a0da8c441ef9
Samuel Mullen <samullen@gmail.com>
2012-03-08 06:49:16 -0600

    20% of restaurants should be disliked by each user.

--- db/migrate/20120308015200_add_restaurants.rb
+++ db/migrate/20120308015200_add_restaurants.rb
@@ -3,7 +3,7 @@ class AddRestaurants < ActiveRecord::Migration
     users = User.all
 
     100000.times do |i|
-      UserRestaurant.create(:user_id => i % 2 == 0 ? users.first.id : users.second.id, :name => "Restaurant #{i}", :like => i % 4 == 0 && false)
+      UserRestaurant.create(:user_id => i % 2 == 0 ? users.first.id : users.second.id, :name => "Restaurant #{i}", :like => i % 5 == 0 ? false : true)
     end
   end
 
